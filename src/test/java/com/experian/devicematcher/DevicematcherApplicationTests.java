4c6166833ab9c23f434acb76774330546309e090
Andre Ghigo <andreghigo@gmail.com>
2025-05-09 01:26:16 -0300

    initial commit

---
+++ src/test/java/com/experian/devicematcher/DevicematcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experian.devicematcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicematcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
