27a38b946ebb4af218a41ddc3d9a9eab08d64510
Andre Ghigo <andreghigo@gmail.com>
2025-05-09 13:02:18 -0300

    API endpoints

---
+++ src/main/java/com/experian/devicematcher/controller/DeviceProfileController.java
@@ -0,0 +1,44 @@
+package com.experian.devicematcher.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/devices")
+public class DeviceProfileController {
+    //logger
+    private static final Logger logger = LoggerFactory.getLogger(DeviceProfileController.class);
+
+    @PostMapping("/")
+    public String matchDeviceProfile(
+            @RequestHeader(value = "User-Agent", required = true) String userAgent
+    ) {
+        // Logic to create a device profile
+        return "Device profile created";
+    }
+
+    @GetMapping("/{deviceId}")
+    public String getDeviceProfileById(
+            @PathVariable(value = "deviceId", required = true) String deviceId
+    ) {
+        // Logic to get a device profile by ID
+        return "Device profile retrieved";
+    }
+
+    @GetMapping("/")
+    public String getDeviceProfiles(
+            @RequestHeader(value = "os-name", required = true) String osName
+    ) {
+        // Logic to get all device profiles
+        return "All device profiles retrieved";
+    }
+
+    @DeleteMapping("/{deviceId}")
+    public String deleteDeviceProfile(
+            @PathVariable(value = "deviceId", required = true) String deviceId
+    ) {
+        // Logic to delete a device profile
+        return "Device profile deleted";
+    }
+}
